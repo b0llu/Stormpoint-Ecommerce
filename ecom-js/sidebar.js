c22249d1182f6faaad96ffe0d3b5d5398ee6fe70
Dhruv <samantdhruv@gmail.com>
2022-02-07 18:20:42 +0530

    fix : sidebar width

--- ecom-js/sidebar.js
+++ ecom-js/sidebar.js
@@ -1,15 +1,18 @@
 const sidebarToggle = document.querySelector("#sidebar-toggle");
 const sidebarMenu = document.querySelector("#sidebar-menu");
 const filterBtn = document.querySelector("#filter-btn");
+const body = document.querySelector('body')
 
 sidebarToggle.addEventListener("click", () => {
   sidebarMenu.classList.toggle("position-open");
   sidebarToggle.classList.toggle("fa-bars");
+  body.classList.toggle("position-fixed")
   filterBtn.innerHTML = "CONFIRM";
 });
 
 filterBtn.addEventListener("click", () => {
   sidebarMenu.classList.toggle("position-open");
   sidebarToggle.classList.toggle("fa-bars");
+  body.classList.remove("position-fixed")
   filterBtn.innerHTML = "FILTERS";
 })

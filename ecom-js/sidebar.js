9983c6b1682e75b6ce712575599150d65deecbb0
Dhruv <samantdhruv@gmail.com>
2022-02-12 12:03:33 +0530

    Revert "added icon on hamburger"
    This reverts commit 5cfefb5b613684bc3c902047384157700cd4d6dc.

--- ecom-js/sidebar.js
+++ ecom-js/sidebar.js
@@ -5,13 +5,11 @@ const filterBtn = document.querySelector("#filter-btn");
 sidebarToggle.addEventListener("click", () => {
   sidebarMenu.classList.toggle("position-open");
   sidebarToggle.classList.toggle("fa-bars");
-  sidebarToggle.classList.toggle("fa-arrow-left");
   filterBtn.innerHTML = "CONFIRM";
 });
 
 filterBtn.addEventListener("click", () => {
   sidebarMenu.classList.toggle("position-open");
   sidebarToggle.classList.toggle("fa-bars");
-  sidebarToggle.classList.toggle("fa-arrow-left");
   filterBtn.innerHTML = "FILTERS";
 })

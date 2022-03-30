00647c299601f34c4d545d31591b6d7ab9928b92
Dhruv <samantdhruv@gmail.com>
2022-03-30 10:15:33 +0530

    refactor : theme js

--- ecom-js/theme.js
+++ ecom-js/theme.js
@@ -1,70 +1,26 @@
 const toggleThemeBtn = document.querySelector("#toggle-theme");
-const bodyDoc = document.querySelector("body");
 
-const rootDoc = document.querySelector(":root");
+const localStorageTheme = localStorage.getItem("theme");
 
-const addDarkTheme = () => {
-    rootDoc.style.setProperty("--default-color", "#101011");
-    rootDoc.style.setProperty("--for-theme", "#ffffff");
-    rootDoc.style.setProperty("--primary-color", "#55E5F5");
-    rootDoc.style.setProperty("--primary-color-dark", "#05cddf");
-    rootDoc.style.setProperty("--secondary-color", "#c3c5c7");
-    rootDoc.style.setProperty("--alert-color", "#e65147");
-    rootDoc.style.setProperty("--success-color", "#4caf50");
-    rootDoc.style.setProperty("--warning-color", "#ff9800");
-    rootDoc.style.setProperty("--nav-bg-color", "#272727");
-    rootDoc.style.setProperty("--header-color", "#238ac5");
-    rootDoc.style.setProperty("--content-color", "#b6bab3");
-    rootDoc.style.setProperty("--card-container-bg", "#f0f0f0");
-    rootDoc.style.setProperty("--text-color", "#1c1b1b");
-    rootDoc.style.setProperty("--icon-color", "#b6bab3");
-};
-
-const addLightTheme = () => {
-    rootDoc.style.setProperty("--default-color", "#e8ecee");
-    rootDoc.style.setProperty("--for-theme", "#171616");
-    rootDoc.style.setProperty("--primary-color", "#238ac5");
-    rootDoc.style.setProperty("--primary-color-dark", "#66e8f6");
-    rootDoc.style.setProperty("--secondary-color", "#c3c5c7");
-    rootDoc.style.setProperty("--alert-color", "#e65147");
-    rootDoc.style.setProperty("--success-color", "#4caf50");
-    rootDoc.style.setProperty("--warning-color", "#ff9800");
-    rootDoc.style.setProperty("--nav-bg-color", "#272727");
-    rootDoc.style.setProperty("--header-color", "#238ac5");
-    rootDoc.style.setProperty("--content-color", "#000000");
-    rootDoc.style.setProperty("--card-container-bg", "#f0f0f0");
-    rootDoc.style.setProperty("--text-color", "#000000");
-    rootDoc.style.setProperty("--icon-color", "#ffffff");
-};
-
-const darkThemeToggleBtn = () => {
-    toggleThemeBtn.classList.remove("fa-moon");
-    toggleThemeBtn.classList.add("fa-sun");
-};
-
-const lightThemeToggleBtn = () => {
-    toggleThemeBtn.classList.add("fa-moon");
-    toggleThemeBtn.classList.remove("fa-sun");
-};
-
-window.addEventListener("DOMContentLoaded", () => {
-    if (localStorage.getItem("theme")) {
-        addDarkTheme();
-        darkThemeToggleBtn();
-    } else {
-        addLightTheme();
-        lightThemeToggleBtn();
-    }
-});
+if (localStorageTheme === "dark") {
+  document.documentElement.setAttribute("data-theme", localStorageTheme);
+  toggleThemeBtn.className = "fas fa-sun icon";
+} else {
+  document.documentElement.setAttribute("data-theme", "light");
+  localStorage.setItem("theme", "light");
+  toggleThemeBtn.className = "fas fa-moon icon";
+}
 
 toggleThemeBtn.addEventListener("click", () => {
-    if (toggleThemeBtn.classList.contains("fa-moon")) {
-        darkThemeToggleBtn();
-        addDarkTheme();
-        localStorage.setItem("theme", "dark");
-    } else {
-        localStorage.removeItem("theme");
-        lightThemeToggleBtn();
-        addLightTheme();
-    }
+  let currentTheme = document.documentElement.getAttribute("data-theme");
+  if (currentTheme === "light") {
+    currentTheme = "dark";
+    toggleThemeBtn.className = "fas fa-sun icon";
+  } else {
+    currentTheme = "light";
+    toggleThemeBtn.className = "fas fa-moon icon";
+  }
+
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  localStorage.setItem("theme", currentTheme);
 });
